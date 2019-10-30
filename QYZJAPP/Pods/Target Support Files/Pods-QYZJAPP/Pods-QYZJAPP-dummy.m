0cc6609d8c7e0cd0ef45de812198342c86fc2183
dorayo <yomingyo@gmail.com>
2019-10-30 14:11:05 +0800

    create app

---
+++ QYZJAPP/Pods/Target Support Files/Pods-QYZJAPP/Pods-QYZJAPP-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QYZJAPP : NSObject
+@end
+@implementation PodsDummy_Pods_QYZJAPP
+@end
---
+++ QYZJAPP/QYZJAPP/AppDelegate.m
@@ -0,0 +1,290 @@
+//
+//  AppDelegate.m
+//  BYXuNiPan
+//
+//  Created by kunzhang on 2018/7/2.
+//  Copyright © 2018年 kunzhang. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "UMessage.h"
+#import <AlipaySDK/AlipaySDK.h>
+#import <UserNotifications/UserNotifications.h>
+#import <WXApi.h>
+#import "TabBarController.h"
+#import "LYGuideViewController.h"
+
+#define UMKey @"567a0b8767e58e04b70070c0"
+//友盟安全密钥//r6xbw5gy0zenei6x56xtm9wmkrrz653y
+#define SinaAppKey @"3443149913"
+#define SinaAppSecret @"2d6bac14bc37989170ba9ab6214f06c3"
+#define WXAppID @"wx013aad9217dedd99"
+#define WXAppSecret @"c8ad8b1d626b309c3f3e3b7b271b11e7"
+#define QQAppID @"1104758682"
+#define QQAppKey @"h97lgfazyRUzXJKy"
+
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [self instantiateRootVC];
+    [self.window makeKeyAndVisible];
+    
+    /* 设置友盟appkey */
+    [[UMSocialManager defaultManager] setUmSocialAppkey:UMKey];
+
+    [self configUSharePlatforms];
+
+    [self confitUShareSettings];
+
+    [self initUment:launchOptions];
+
+    // 发送崩溃日志
+    
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    
+    NSString *dataPath = [path stringByAppendingPathComponent:@"Exception.txt"];
+    
+    NSData *data = [NSData dataWithContentsOfFile:dataPath];
+    
+    if (data != nil) {
+        
+//        [self sendExceptionLogWithData:data path:dataPath];
+        
+    }
+    
+    return YES;
+}
+
+- (void)configUSharePlatforms
+{
+    /* 设置微信的appKey和appSecret */
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_WechatSession appKey:WXAppID appSecret:WXAppSecret redirectURL:@"http://mobile.umeng.com/social"];
+    /*
+     * 移除相应平台的分享，如微信收藏
+     */
+    //[[UMSocialManager defaultManager] removePlatformProviderWithPlatformTypes:@[@(UMSocialPlatformType_WechatFavorite)]];
+
+    /* 设置分享到QQ互联的appID
+     * U-Share SDK为了兼容大部分平台命名，统一用appKey和appSecret进行参数设置，而QQ平台仅需将appID作为U-Share的appKey参数传进即可。
+     */
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_QQ appKey:QQAppID/*设置QQ平台的appID*/  appSecret:QQAppKey redirectURL:@"http://mobile.umeng.com/social"];
+
+    /* 设置新浪的appKey和appSecret */
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Sina appKey:SinaAppKey  appSecret:SinaAppSecret redirectURL:@"https://sns.whalecloud.com/sina2/callback"];
+
+
+}
+
+- (void)initUment:(NSDictionary *)launchOptions{
+    //友盟适配https
+    [UMessage startWithAppkey:UMKey launchOptions:launchOptions httpsEnable:YES];
+    //    [UMessage startWithAppkey:UMKey launchOptions:launchOptions];
+    [UMessage registerForRemoteNotifications];
+    
+    //iOS10必须加下面这段代码。
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate=self;
+    UNAuthorizationOptions types10=UNAuthorizationOptionBadge|  UNAuthorizationOptionAlert|UNAuthorizationOptionSound;
+    [center requestAuthorizationWithOptions:types10     completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+            //点击允许
+            //这里可以添加一些自己的逻辑
+        } else {
+            //点击不允许
+            //这里可以添加一些自己的逻辑
+        }
+    }];
+    
+    //打开日志，方便调试
+    [UMessage setLogEnabled:YES];
+}
+
+- (void)confitUShareSettings
+{
+    /*
+     * 打开图片水印
+     */
+    //[UMSocialGlobal shareInstance].isUsingWaterMark = YES;
+    
+    /*
+     * 关闭强制验证https，可允许http图片分享，但需要在info.plist设置安全域名
+     <key>NSAppTransportSecurity</key>
+     <dict>
+     <key>NSAllowsArbitraryLoads</key>
+     <true/>
+     </dict>
+     */
+    //[UMSocialGlobal shareInstance].isUsingHttpsWhenShareContent = NO;
+    
+}
+
+
+//在用户接受推送通知后系统会调用
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+//    self.pushToken = deviceToken;
+//    if (![LxmTool ShareTool].isClosePush)
+//    {
+//        [UMessage registerDeviceToken:deviceToken];
+//        //2.获取到deviceToken
+//        NSString *token = [[[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]] stringByReplacingOccurrencesOfString:@" " withString:@""];
+//        //将deviceToken给后台
+//        NSLog(@"send_token:%@",token);
+//        [LxmTool ShareTool].deviceToken = token;
+//        //[[LxmTool ShareTool] uploadDeviceToken];
+//    }
+//    else
+//    {
+//        [UMessage registerDeviceToken:nil];
+//        [LxmTool ShareTool].deviceToken = @"";
+//        //[[LxmTool ShareTool] uploadDeviceToken];
+//    }
+    
+    
+}
+
+
+//设置根视图控制器
+- (UIViewController *)instantiateRootVC{
+    
+    //没有引导页
+    //    zkRootVC *BarVC=[[zkRootVC alloc] init];
+    //    return BarVC;
+    
+    
+    
+    //获取app运行的版本号
+    NSString *currentVersion =[[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
+    //取出本地缓存的版本号
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *localVersion = [defaults objectForKey:@"appversion"];
+    if ([currentVersion isEqualToString:localVersion]) {
+        TabBarController *BarVC=[[TabBarController alloc] init];
+        return BarVC;
+        //        TabBarController * tabVc = [[TabBarController alloc] init];
+        //        return tabVc;
+        
+    }else{
+        LYGuideViewController *guideVc = [[LYGuideViewController alloc] init];
+        return guideVc;
+    }
+}
+//跳转主页
+- (void)showHomeVC{
+    TabBarController  *BarVC=[[TabBarController alloc] init];
+    self.window.rootViewController = BarVC;
+    
+    //更新本地储存的版本号
+    NSString *currentVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
+    [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:@"appversion"];
+    //同步到物理文件存储
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+
+#pragma mark -支付宝 微信支付
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    //跳转到支付宝支付的情况
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            //发送一个通知
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"ZFBPAY" object:resultDic];
+            NSLog(@"result ======================== %@",resultDic);
+        }];
+    } else if ([url.absoluteString hasPrefix:@"wx013aad9217dedd99://pay"] ) {
+        //微信
+        [WXApi handleOpenURL:url delegate:self];
+        
+    }else {//友盟
+        [[UMSocialManager defaultManager] handleOpenURL:url];
+    }
+    return YES;
+    
+}
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+    //跳转到支付宝支付的情况
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            //发送一个通知,告诉支付界面要做什么
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"ZFBPAY" object:resultDic];
+            NSLog(@"result ======================== %@",resultDic);
+        }];
+    } else if ([url.absoluteString hasPrefix:@"wx013aad9217dedd99://pay"] ) {
+        
+        [WXApi handleOpenURL:url delegate:self];
+        
+        
+    }else {
+        [[UMSocialManager defaultManager] handleOpenURL:url];
+    }
+    
+    return YES;
+}
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+    //跳转到支付宝支付的情况
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            //发送一个通知
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"ZFBPAY" object:resultDic];
+            
+            NSLog(@"result ======================== %@",resultDic);
+        }];
+    } else if ([url.absoluteString hasPrefix:@"wx013aad9217dedd99://pay"] ) {
+        [WXApi handleOpenURL:url delegate:self];
+        
+    }else {
+        [[UMSocialManager defaultManager] handleOpenURL:url options:options];
+    }
+    return YES;
+}
+//微信支付结果
+- (void)onResp:(BaseResp *)resp {
+    //发送一个通知
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"WXPAY" object:resp];
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+@end
