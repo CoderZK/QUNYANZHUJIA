d333952e92df7d2f2161b5168a918e4af4ef9c35
dorayo <yomingyo@gmail.com>
2019-11-15 17:31:56 +0800

    no message

---
+++ QYZJAPP/QYZJAPP/我的/我的钱包/vcs/QYZJMineWalletTVC.m
@@ -0,0 +1,33 @@
+
+//
+//  QYZJMineWalletTVC.m
+//  QYZJAPP
+//
+//  Created by zk on 2019/11/15.
+//  Copyright © 2019 kunzhang. All rights reserved.
+//
+
+#import "QYZJMineWalletTVC.h"
+
+@interface QYZJMineWalletTVC ()
+
+@end
+
+@implementation QYZJMineWalletTVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
