ec23eac59041c119218aa614a8f6479562abb1a4
dorayo <yomingyo@gmail.com>
2020-04-22 10:10:18 +0800

    ppp

--- QYZJAPP/QYZJAPP/首页/VC/QYZJCreateShiGongQingDanTVC.m
+++ QYZJAPP/QYZJAPP/首页/VC/QYZJCreateShiGongQingDanTVC.m
@@ -402,7 +402,6 @@
                 vc.hidesBottomBarWhenPushed = YES;
                 [self.navigationController pushViewController:vc animated:YES];
             }else {
-               
                 [SVProgressHUD showSuccessWithStatus:@"操作成功"];
                 dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                     
